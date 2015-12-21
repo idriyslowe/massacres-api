96e4c89cef32b37afd10fe7dbfc914c891fc87a3
idriyslowe <idriyswins@gmail.com>
2015-12-21 17:45:36 -0600

    fixed delete action in incidents controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/incidents' => 'incidents#index'
   get '/incidents/:id' => 'incidents#show'
   post '/incidents' => 'incidents#create'
+  delete '/incidents/:id' => 'incidents#destroy'
 end
