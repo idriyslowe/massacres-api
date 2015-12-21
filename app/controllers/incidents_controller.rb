6b9b9e53c6023d18b27bdca26cf7b3d61d82f721
idriyslowe <idriyswins@gmail.com>
2015-12-21 17:10:14 -0600

    ?

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -5,6 +5,21 @@ class IncidentsController < ApplicationController
   def index
     @incidents = Incident.all
   end
+  def edit
+    @incident = Incident.find_by(:id params[:id])
+  end
+  def update
+    @incident = Incident.find_by(:id params[:id])
+    @incident.update(
+      murderer_first_name: params[:murderer_first_name],
+      murderer_last_name: params[:murderer_last_name],
+      birthdate: params[:birthdate],
+      location: params[:location],
+      weapon: params[:weapon],
+      number_of_victims: params[:number_of_victims],
+      sentence: params[:sentence]
+      )
+  end
   def create
     @incident = Incident.create(
       murderer_first_name: params[:murderer_first_name],
@@ -16,5 +31,9 @@ class IncidentsController < ApplicationController
       sentence: params[:sentence]
       )
     render :show
+  end
+  def destroy
+    incident = Incident.find_by(id: params[:id])
+    incident.destroy
   end
 end
